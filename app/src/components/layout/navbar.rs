e1d7df003aeb40d96cc8e564fab3a393fda7f806
Dmitriy V. Isaev <shtirlizlive@yandex.ru>
2026-06-08 22:50:07 +0300

    show error

--- app/src/components/layout/navbar.rs
+++ app/src/components/layout/navbar.rs
@@ -1,7 +1,7 @@
 use leptos::prelude::*;
 use leptos_router::hooks::use_query_map;
 
-use crate::components::layout::message_banner::{Messages, show_info};
+use crate::components::layout::message_banner::{Messages, show_error, show_info};
 use crate::components::ui::button::Button;
 use crate::components::ui::button_link::ButtonLink;
 use crate::domain::home::routing::routes::HomeRoutes;
@@ -23,9 +23,14 @@ pub fn Navbar() -> impl IntoView {
     provide_context(user_resource);
 
     Effect::new(move |_| {
-        if let Some(Ok(_)) = logout.value().get() {
-            show_info("Вы вышли!".to_owned(), messages);
-            logout.clear();
+        if let Some(res) = logout.value().get() {
+            match res {
+                Ok(_) => {
+                    show_info("Вы вышли!".to_owned(), messages);
+                    logout.clear();
+                }
+                Err(err) => show_error(err.to_string(), messages),
+            }
         }
     });
 
